6669be76880f7a32bf9e9ae28cac7273d2ad4824
Nyaundi <jamesmabuka260@gmail.com>
2023-08-30 19:12:57 +0300

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,71 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  title: 'JS.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'James Nyaundi',
+  role: 'DevOps Engineer and Frontend Engineer',
+  description:
+    '“DevOps is not a goal, but a never-ending process of continual improvement” (Jez Humble)',
+  resume: 'https://docs.google.com/document/d/1KG9M5GMmG_xxcx5pXJhWM7upBkJlNuwX/edit?pli=1',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/james-nyaundi-2229611b3/',
+    github: 'https://github.com/Nyaundi',
+  },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'Project 1',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 2',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 3',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Redux',
+  'SASS',
+  'Material UI',
+  'Git',
+  'CI/CD',
+  'Jest',
+  'Enzyme',
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'johnsmith@mail.com',
+}
+
+export { header, about, projects, skills, contact }
